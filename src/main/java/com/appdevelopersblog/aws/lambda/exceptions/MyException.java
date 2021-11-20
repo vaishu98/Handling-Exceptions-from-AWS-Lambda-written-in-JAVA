444899eddd4a26ce13eaa7334ebff11437d1cf38
Vishnu Priya Chilakamarthi <vishnu.avvs@gmail.com>
2021-11-20 21:50:56 +0530

    initial commit

--- src/main/java/com/appdevelopersblog/aws/lambda/exceptions/MyException.java
+++
@@ -1,9 +0,0 @@
-package com.appdevelopersblog.aws.lambda.exceptions;
-
-public class MyException extends RuntimeException{
-
-    public MyException(String message) {
-        super(message);
-    }
-
-}
